f7dfd7228b4d2689963194ca3802e1b578929dd0
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-05-08 13:54:43 +0000

    Create base.html and index.html

--- runningstories/urls.py
+++ runningstories/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from stories import views as story_views
+from django.urls import path, include
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
-    path('stories/', story_views.my_story, name='stories'),
+    path('', include("stories.urls"), name="stories.urls"),
 ]
--- stories/views.py
+++ stories/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def my_story(request):
-    return HttpResponse("Hello, Runners!")
+    context = {
+        'message': "Hello, hello!",
+    }
+
+    return render(request, 'index.html', context)
