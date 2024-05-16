48f751b38920a003248003f7dd09fd62ae32144c
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-05-16 10:39:31 +0000

    Create Comment model

--- stories/models.py
+++ stories/models.py
@@ -19,6 +19,18 @@ class Story(models.Model):
     def __str__(self):
         return f"{self.title} written by {self.author}"
 
+class Comment(models.Model):
+    story = models.ForeignKey(
+        Story, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='commenter'
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
 class About(models.Model):
     title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
