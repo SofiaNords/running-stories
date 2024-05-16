3c0a7432efaaf01a3dd75079525a58d1b7e78dfb
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-05-16 08:09:41 +0000

    Install Summernote for About page

--- stories/admin.py
+++ stories/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Story, About
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(About)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content',)
 
 # Register your models here.
 admin.site.register(Story)
-admin.site.register(About)
