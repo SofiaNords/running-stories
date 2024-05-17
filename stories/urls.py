bf0e5a8f3b72154b2d49e088746800f2c1a717e1
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-05-17 05:41:03 +0000

    Add edit and delete logic to comments

--- stories/urls.py
+++ stories/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
         views.story_edit, name='story_edit'),
     path('share-story/story_delete/<int:story_id>',
         views.story_delete, name='story_delete'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+        views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]
