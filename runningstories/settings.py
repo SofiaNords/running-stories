7e42a37a07978859716d24cb0686f8fadbce38fe
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-05-22 12:10:31 +0000

    Create footer in base.html

--- runningstories/settings.py
+++ runningstories/settings.py
@@ -28,7 +28,6 @@ SECRET_KEY = 'django-insecure-e)gwxkrk^%wuy)t&kt5x*)6q#_2093tfquhkh60250(!#gxo2x
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
 ALLOWED_HOSTS = ['8000-sofianords-runningstori-4js02sl7agk.ws-eu114.gitpod.io', '.herokuapp.com']
 
 # Application definition
