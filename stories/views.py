aca5ae2eff3330cac415d72dc2e61a5d64750220
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-05-24 05:57:57 +0000

    Comment views.py

--- stories/views.py
+++ stories/views.py
@@ -1,9 +1,9 @@
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views import generic
 from django.utils.text import slugify
-from django.http import HttpResponseRedirect
 from .models import Story, About, Comment
 from .forms import CreateNewStory, CommentForm
 
@@ -11,8 +11,10 @@ def about(request):
     """
     Renders the About page
     """
+    # Retrieve the latest About object from the database
     about = About.objects.all().order_by('-updated_on').first()
 
+    # Render the "about.html" page with the About object as data
     return render(
         request,
         "stories/about.html",
@@ -21,26 +23,33 @@ def about(request):
 
 class StoryList(generic.ListView):
     """
-    View to view a list of stories
+    View to display a list of stories
     """
-    queryset = Story.objects.all() # Queryset to retrieve all Story objects
-    template_name = 'stories/index.html' # Template file for rendering the view
-    context_object_name = 'story_list' # Name of the context variable in the template
+    # Queryset containing all Story objects
+    queryset = Story.objects.all()
+    # Template to render for this view
+    template_name = 'stories/index.html'
+    # Name of the context variable in the template
+    context_object_name = 'story_list'
+    # Number of stories to display per page (pagination)
     paginate_by = 4
 
 
 def story_detail(request, slug):
     """
-    View to view the story posts in detail
+    View to display the story posts in detail
     """
-    queryset = Story.objects.filter(status=1) # Queryset to retrieve Story objects with status=1
-    print("Visa alla stories")
-    story = get_object_or_404(queryset, slug=slug) # Get the Story object with the specified slug
-    print("gå till storyn med rätt slug")
+    # Get all published stories (status=1)
+    queryset = Story.objects.filter(status=1)
+    # Retrieve the story with the specified slug
+    story = get_object_or_404(queryset, slug=slug) 
+    # Get all comments for this story, ordered by creation date
     comments = story.comments.all().order_by("-created_on")
+    # Count the number of approved comments
     comment_count = story.comments.filter(approved=True).count(
     )
 
+    # Handle form submission for adding new comments
     if request.method == "POST":
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
@@ -49,36 +58,44 @@ def story_detail(request, slug):
             comment.story = story
             comment.save()
 
+    # Initialize an empty comment form
     comment_form = CommentForm()
 
+    # Render the "story_detail.html" template with relevant data
     return render(
         request,
-        "stories/story_detail.html", # Template file for rendering the view
+        "stories/story_detail.html", 
         {"story": story,
         "comments": comments,
         "comment_count": comment_count,
         "comment_form": comment_form,
-        },  # Context variable containing the Story object
+        }, 
     )
 
 @login_required
 def share_story(request):
     """
-    View to post stories and view your own stories under My Stories
+    View to post stories and view your own stories under Share Story
     """
+    # Handle form submission for sharing a new story
     if request.method == 'POST':
         share_form = CreateNewStory(request.POST)
         if share_form.is_valid():
+            # Create a new story object from the form data
             story = share_form.save(commit=False)
             story.author = request.user
             story.slug = slugify(story.title)
             story.save()
+            # Redirect to the 'share' URL after successful submission
             return redirect('share')
     else:
+        # Initialize an empty share form
         share_form = CreateNewStory()
 
+    # Get all stories (for display in 'My Stories')
     my_stories = Story.objects.all()
 
+    # Render the 'share_story.html' template with relevant data
     return render(
         request, 'stories/share_story.html', {"share_form": share_form, "my_stories": my_stories}
     )
@@ -86,84 +103,110 @@ def share_story(request):
 @login_required
 def story_edit(request, story_id):
     """
-    View to edit story
+    View to edit a story
     """
     if request.method == "POST":
 
+        # Collecting stories with status 1
         queryset = Story.objects.filter(status=1)
-        print("Collection of stories")
+        # Fetching the story object based on the story_id
         story = get_object_or_404(Story, pk=story_id)
-        print("Get object")
+        # Creating a new story form with the POST data and the fetched story instance
         share_form = CreateNewStory(data=request.POST, instance=story)
-        print("Create new story")
 
         if share_form.is_valid() and story.author == request.user:
+            # Saving the form but not committing to the database yet
             story = share_form.save(commit=False)
-            print("Save later")
+            # Setting the author of the story to the current user
             story.author = request.user
-            print("författaren = användare")
+            # Creating a URL-friendly version of the story title
             story.slug = slugify(story.title)
-            print("Slug")
+            # Saving the story to the database
             story.save()
-            print("Spara")
+            # Displaying a success message
             messages.add_message(request, messages.SUCCESS, 'Story Updated!')
-            print("Storyn är uppdatderad!")
         else:
+            # Displaying an error message if the form is not valid
             messages.add_message(request, messages.ERROR, 'Error updating story!')
 
+    # Redirecting to the 'share' view
     return HttpResponseRedirect(reverse('share'))
 
 @login_required
 def story_delete(request, story_id):
     """
-    view to delete story
+    View to delete a story
     """
+    # Collecting stories with status 1
     queryset = Story.objects.filter(status=1)
+    # Fetching the story object based on the story_id
     story = get_object_or_404(Story, pk=story_id)
 
     if story.author == request.user:
+        # Deleting the story if the current user is the author
         story.delete()
+        # Displaying a success
         messages.add_message(request, messages.SUCCESS, 'Story deleted!')
     else:
+        # Displaying an error message if the current user is not the author
         messages.add_message(request, messages.ERROR, 'You can only delete your own stories!')
 
+    # Redirecting to the 'share' view
     return HttpResponseRedirect(reverse('share'))
 
 
 def comment_edit(request, slug, comment_id):
     """
-    view to edit comments
+    View to edit comments
     """
     if request.method == "POST":
 
+        # Collecting stories with status 1
         queryset =Story.objects.filter(status=1)
+        # Fetching the story object based on the slug
         story = get_object_or_404(queryset, slug=slug)
+        # Fetching the comment object based on the comment_id
         comment = get_object_or_404(Comment, pk=comment_id)
+        # Creating a new comment form with the POST data and the fetched comment instance
         comment_form = CommentForm(data=request.POST, instance=comment)
 
         if comment_form.is_valid() and comment.author == request.user:
+            # Saving the form but not committing to the database yet
             comment = comment_form.save(commit=False)
+            # Associating the comment with the fetched story
             comment.story = story
+            # Setting the comment as not approved
             comment.approved = False
+            # Saving the comment to the database
             comment.save()
+            # Displaying a success message
             messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
         else:
+            # Displaying an error message if the form is not valid
             messages.add_message(request, messages.ERROR, 'Error updating comment!')
 
+    # Redirecting to the 'story_detail' view with the story slug as an argument
     return HttpResponseRedirect(reverse('story_detail', args=[slug]))
 
 def comment_delete(request, slug, comment_id):
     """
-    view to delete comment
+    View to delete a comment
     """
+    # Collecting stories with status 1
     queryset = Story.objects.filter(status=1)
+    # Fetching the story object based on the slug
     story = get_object_or_404(queryset, slug=slug)
+    # Fetching the comment object based on the comment_id
     comment = get_object_or_404(Comment, pk=comment_id)
 
     if comment.author == request.user:
+        # Deleting the comment if the current user is the author
         comment.delete()
+        # Displaying a success message
         messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
     else:
+        # Displaying an error message if the current user is not the author
         messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
 
+    # Redirecting to the 'story_detail' view with the story slug as an argument
     return HttpResponseRedirect(reverse('story_detail', args=[slug]))
