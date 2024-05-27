0b34b9aa553f71caebf6f0b117c853962a44de74
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-05-27 07:30:07 +0000

    Fix CI Python Linter warnings in admin.py

--- stories/admin.py
+++ stories/admin.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from .models import Story, About, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
+
 @admin.register(About)
 class PostAdmin(SummernoteModelAdmin):
-
     summernote_fields = ('content',)
 
+
 # Register your models here.
+
 admin.site.register(Story)
 admin.site.register(Comment)
