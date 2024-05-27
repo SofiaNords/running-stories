7b766ffd412fe1410945051744b434d86c27edf4
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-05-27 07:33:27 +0000

    Fix CI Python Linter warnings in forms.py

--- stories/forms.py
+++ stories/forms.py
@@ -1,10 +1,12 @@
 from .models import Story, Comment
 from django import forms
 
+
 class CreateNewStory(forms.ModelForm):
     class Meta:
         model = Story
         fields = ('title', 'excerpt', 'content', 'status')
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
