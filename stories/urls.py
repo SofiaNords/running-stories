b1295d706ed5f0d96a0aaad27029df191a7cfdcc
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-05-27 07:49:38 +0000

    Fix CI Python Linter warnings in stories/urls.py

--- stories/urls.py
+++ stories/urls.py
@@ -2,16 +2,24 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.StoryList.as_view(), name= 'home'),
+    path('', views.StoryList.as_view(), name='home'),
     path('about/', views.about, name='about'),
-    path('share-story/', views.share_story, name= 'share'),  
+    path('share-story/', views.share_story, name='share'),
     path('<slug:slug>/', views.story_detail, name='story_detail'),
-    path('share-story/edit_story/<int:story_id>',
-        views.story_edit, name='story_edit'),
-    path('share-story/story_delete/<int:story_id>',
-        views.story_delete, name='story_delete'),
-    path('<slug:slug>/edit_comment/<int:comment_id>',
-        views.comment_edit, name='comment_edit'),
-    path('<slug:slug>/delete_comment/<int:comment_id>',
-         views.comment_delete, name='comment_delete'),
+    path(
+        'share-story/edit_story/<int:story_id>',
+        views.story_edit, name='story_edit'
+        ),
+    path(
+        'share-story/story_delete/<int:story_id>',
+        views.story_delete, name='story_delete'
+        ),
+    path(
+        '<slug:slug>/edit_comment/<int:comment_id>',
+        views.comment_edit, name='comment_edit'
+        ),
+    path(
+        '<slug:slug>/delete_comment/<int:comment_id>',
+        views.comment_delete, name='comment_delete'
+        ),
 ]
